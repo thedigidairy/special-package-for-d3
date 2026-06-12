96182e9bc1b02c2f2adedd9b80ed8acf42b2022b
thedigidairy <thedigidairy@outlook.com>
2026-06-12 11:35:13 +0530

    Build interactive D3 birthday surprise website

---
+++ tailwind.config.js
@@ -0,0 +1,2 @@
+/** @type {import('tailwindcss').Config} */
+export default { content:['./index.html','./src/**/*.{js,jsx}'], theme:{extend:{fontFamily:{serif:['Georgia','serif'],hand:['"Comic Sans MS"','cursive']}}}, plugins:[] }
